a58b26679db55468b0205c7137fcb161bc436f24
vzaidman <vzaidman@gmail.com>
2019-06-10 17:56:14 +0300

    added publications

--- build.js
+++ build.js
@@ -3,13 +3,22 @@ const processCss = require('./build/process-css')
 
 let html = fs.readFileSync('./src/index.html').toString()
 
-const projects = JSON.parse(fs.readFileSync('./data/project-list.json').toString())
+const projectsList = JSON.parse(fs.readFileSync('./data/project-list.json').toString())
 const projectListItemTemplate = fs.readFileSync('./src/templates/project-list-item.html').toString()
 html = require('./build/process-list-template')({
 	htmlContent: html,
-	listItemsData: projects,
+	listItemsData: projectsList,
 	templateName: 'project-list',
 	listItemTemplate: projectListItemTemplate
+})
+
+const publicationList = JSON.parse(fs.readFileSync('./data/publication-list.json').toString())
+const publicationListItemTemplate = fs.readFileSync('./src/templates/publication-list-item.html').toString()
+html = require('./build/process-list-template')({
+	htmlContent: html,
+	listItemsData: publicationList,
+	templateName: 'publication-list',
+	listItemTemplate: publicationListItemTemplate
 })
 
 const css = fs.readFileSync('./src/index.css').toString()
