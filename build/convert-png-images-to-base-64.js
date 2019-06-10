db0b2bb9c5b52fdc5d7bde19b819c277d0ccc3f9
vzaidman <vzaidman@gmail.com>
2019-06-10 18:52:04 +0300

    social media icons

--- build/convert-png-images-to-base-64.js
+++ build/convert-png-images-to-base-64.js
@@ -1,9 +1,8 @@
 const fs = require('fs')
-const path = require('path')
 
 module.exports = html => {
 	return html.replace(/src=\"([^"]*png)\"/g, (match, imagePath) => {
-		const imageAsBase64 = fs.readFileSync(path.resolve('./src/', imagePath), 'base64');
+		const imageAsBase64 = fs.readFileSync(`./src/assets/${imagePath}`, 'base64');
 		return `src="data:image/jpeg;base64,${imageAsBase64}"`
 	})
 }
