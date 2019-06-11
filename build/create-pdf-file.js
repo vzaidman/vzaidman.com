6e1813563621af739d8a8e96f3b489c1887f8220
vzaidman <vzaidman@gmail.com>
2019-06-11 20:20:25 +0300

    cv button improvement

--- build/create-pdf-file.js
+++ build/create-pdf-file.js
@@ -1,10 +1,14 @@
 const fs = require('fs')
 const htmlPdf = require('html-pdf-chrome');
 
-module.exports = async html => {
+module.exports = html => {
+	const interval = setInterval(() => console.log('building pdf cv'), 2000)
+
 	html = html.replace(/<footer\b[^>]*>.*?<\/footer>/, '') // remove footer
 	html = html.replace(/<a download\b[^>]*>.*?<\/a>/, '') // remove the download cv button
 
-	const pdf = await htmlPdf.create(html);
-	pdf.toFile('dist/Vitali_Zaidman_CV.pdf');
+	const pdf = htmlPdf.create(html)
+		.then(pdf => pdf.toFile('dist/Vitali_Zaidman_CV.pdf'))
+		.catch((...args) => console.error(...args))
+		.finally(() => clearInterval(interval))
 }
