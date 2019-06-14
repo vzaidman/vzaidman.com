0dbba2975790ef5b11db160bc904420b73ebefbe
vzaidman <vzaidman@gmail.com>
2019-06-14 10:45:12 +0300

    improved CV info

--- build/create-pdf.js
+++ build/create-pdf.js
@@ -2,10 +2,14 @@ const fs = require('fs')
 const htmlPdf = require('html-pdf-chrome');
 
 module.exports = html => {
-	const interval = setInterval(() => console.log('building pdf cv'), 2000)
+	const interval = setInterval(() => console.log('building pdf cv...'), 1000)
 
+	// copy it to assets for the time being.
+	// the better way would be to create it in build time
+	// instead of re-creating it but zeit pipelines doesn't
+	// support it
 	const pdf = htmlPdf.create(html)
-		.then(pdf => pdf.toFile('dist/Vitali_Zaidman_CV.pdf'))
+		.then(pdf => pdf.toFile('assets/Vitali_Zaidman_CV.pdf'))
 		.catch((...args) => console.error(...args))
 		.then(() => clearInterval(interval))
 }
