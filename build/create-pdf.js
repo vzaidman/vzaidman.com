58f5a718d6d4c7541ed1612237798ff0fc06ad08
vzaidman <vzaidman@gmail.com>
2019-06-14 10:09:07 +0300

    improved the "download-cv" button

--- build/create-pdf.js
+++ build/create-pdf.js
@@ -3,7 +3,7 @@ const htmlPdf = require('html-pdf-chrome');
 
 module.exports = html => {
 	const interval = setInterval(() => console.log('building pdf cv'), 2000)
-	
+
 	const pdf = htmlPdf.create(html)
 		.then(pdf => pdf.toFile('dist/Vitali_Zaidman_CV.pdf'))
 		.catch((...args) => console.error(...args))
