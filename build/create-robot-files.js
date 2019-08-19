5f051bd69b475651d65b3d35e2a48ab66704a9ae
vzaidman <vzaidman@gmail.com>
2019-08-19 17:54:53 +0300

    new cv

--- build/create-robot-files.js
+++ build/create-robot-files.js
@@ -4,7 +4,7 @@ module.exports = () => {
 	fs.writeFileSync('dist/robots.txt', [
 		'user-agent: *',
 		'allow: /$',
-		'allow: /vitali_zaidman_cv.pdf$',
+		'allow: /vitali-zaidman-cv.pdf$',
 		'allow: /vitali-zaidman-200x200.png$',
 		'',
 		'sitemap: https://vzaidman.com/sitemap.xml$'
